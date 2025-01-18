21a13608a333a9f04245b3730716508c07dc864e
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-01-18 20:08:24 +0530

    Add files via upload

---
+++ Contractor_Repository.java
@@ -0,0 +1,9 @@
+package in.sb.main.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import in.sb.main.entity.Contractor;
+
+public interface Contractor_Repository extends JpaRepository<Contractor, Integer>{
+
+}
