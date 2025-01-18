933205cc8a65d649ce7ac03898830f87684bf969
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-01-18 20:09:25 +0530

    Add files via upload

---
+++ CrudContractorApplication.java
@@ -0,0 +1,13 @@
+package in.sb.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudContractorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudContractorApplication.class, args);
+	}
+
+}
