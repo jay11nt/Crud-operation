e13ff16fd710bb4d3b0913679a6c4b65d22948ec
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-01-18 21:30:30 +0530

    Delete CRUD_Employee directory

--- CRUD_Employee/CRUD_Contractor/src/main/java/in/sb/main/repo/Contractor_Repository.java
+++
@@ -1,9 +0,0 @@
-package in.sb.main.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import in.sb.main.entity.Contractor;
-
-public interface Contractor_Repository extends JpaRepository<Contractor, Integer>{
-
-}
